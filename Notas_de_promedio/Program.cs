524af991499fefb9ac9eaa257154a6c0a9cf2727
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-16 19:22:01 -0600

    Agregar archivos de proyecto.

---
+++ Notas_de_promedio/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
