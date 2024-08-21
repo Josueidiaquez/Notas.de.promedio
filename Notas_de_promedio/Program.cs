e6610598c72a2272290246141781a947ea574005
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-21 13:56:29 -0600

    Update Program.cs

--- Notas_de_promedio/Program.cs
+++ Notas_de_promedio/Program.cs
@@ -5,3 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
+namespace notasdepromedio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int ap = 0;
+            float nota, acAprobadas = 0, acReprobadas = 0;
+
+            for (int x = 0; x < 40; x++)
+            {
+                Console.WriteLine("Favor ingresar Nota: ");
+                nota = float.Parse(Console.ReadLine());
+
+                if (nota >= 3)
+                {
+                    acAprobadas = acAprobadas + nota;
+                    ap++;
+                }
+                else
+                    acReprobadas = acReprobadas + nota;
+
+            }
+            Console.WriteLine("El promedio de notas Aprobadas es: " + acAprobadas / ap);
+            Console.WriteLine("El promedio de notas Reprobadas es: " + acReprobadas / (40 - ap));
+            Console.ReadKey();
+        }
+    }
+
+}
